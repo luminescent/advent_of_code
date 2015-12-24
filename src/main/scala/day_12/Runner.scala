8ba2f874aa0933a9829a36b711a0f6e8d743b1a7
cnicolau <cnicolau@travelrepublic.co.uk>
2015-12-24 11:45:03 +0000

    day 12

---
+++ src/main/scala/day_12/Runner.scala
@@ -0,0 +1,16 @@
+package day_12
+
+import common.InputReader
+
+/**
+ * Created by christina on 24/12/15.
+ */
+object Runner extends App {
+  
+  val input = InputReader read "/home/christina/IdeaProjects/advent_of_code/src/main/scala/day_12/A.txt"
+  //println(s"Total: ${Numberer sum input}")
+
+  println(s"Total without red properties: ${Numberer sumWithoutRed input}")
+
+
+}
